3743d6a8fac814c26fa3cbc887c330e287280cae
Daryl Kranec <dkranec@gmail.com>
2018-04-25 18:32:14 -0400

    Scanner Microservice Created

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
  - `git clone https://github.com/dkran/hydra-scanner`
  - `cd hydra-scanner`
  - `npm install`
- - `node app.js`
+ - `sudo node app.js`
 
 #### What is this for?
  - Microservice to facilitate scanning the entire internet.
- - Scans an IP.  I will add command line arguments to allow you to scan riskier servers (govt's, DPRK?) with an include file later.
- - I will try my hardest to exclude those for now ;) 
+ - Scans an IP.  Don't worry, I have a fairly comprehensive exclude file, so my server should send you nothing that would be risky.
  - Reports back to a [`hydra-backend`](https://github.com/dkran/hydra-backend) server.  Doesn't really matter which one, but I will be setting up a main one soon as default.
- - Only scans a single IP.  On the backend masscan will gather ips' for this microservice to scan in full
+ - Only scans a single IP.  On the backend masscan will gather ips' for this microservice to scan in full.  I later hope to use parallel scanning / threads to do this, but I'm reading Fyodor's book on nmap to get it right.
 
  
--- app.js
+++ app.js
@@ -1,47 +1,65 @@
 const spawn = require('child_process').spawn,
-  processNmap = require('nmap2json'),
+  processNmap = require('../nmap2json'),
   fs = require('fs'),
-  WebSocket = require('ws'),
+  Websocket = require('ws'),
   inspect = require('util').inspect;
-/*const ws = new Websocket('wss://localhost:3000', {
-  origin: 'http://localhost'
-})*/
-/*
-ws.on('open', ()=>{
-  ws.send(JSON.stringify({type: 'getIps', from: 'scanner', data: ips.length}))
-})
-
-ws.on('scanIPdata', (data)=>{
-  var data = JSON.parse(data)
-  for(var i=0; i<data.length; i++){
-    ips.push(data.shift())
-  }
-})*/
+const ws = new Websocket('ws://localhost:3000')
 
 var ips = []
-var count = 0
-var ip = ip || 'localhost'
-var xml = (__dirname + '/xml/' + ip)
-setInterval(()=>{
-  if(count < 10) {
-    const nmap = spawn('nmap', ['-sS', '-A', '-p-','-oX', xml, ips.shift()])
-    nmap.setMaxListeners(20)
-    nmap.stderr.on('error', (err) => {
-      log.info(error)
-    })
-    nmap.on('close', (data) => {
-      count--
-    })
+
+ws.on('open', () => {
+  console.log('sending request for ips')
+  ws.send(JSON.stringify({ type: 'getIps', from: 'scanner', data: ips.length }))
+})
+  
+ws.on('message', (data) => {
+  data = JSON.parse(data)
+  console.log(data)
+  if (data.type === 'ipData') {
+    for (var i = 0; i < data.data.length; i++) {
+      ips.push(data.data.shift())
+    }
   }
-},1000)
+})
+ws.on('close', ()=>{
+  console.log('connection closed')
+})
 
-
-
-
-var xmlFiles = fs.readdirSync(__dirname + '/xml/')
-  for(var j = 0; j< xml.length; j++){
-    processNmap(fs.readFileSync(__dirname + '/xml/' + xml[j])).then((result)=>{
-      console.log(inspect(result))
-    })
+setInterval(() => {
+  console.log('checking scan progress')
+  var count = 0
+  if (count < 10) {
+    if (ips.length > 0) {
+      var xml = (__dirname + '/xml/' + ips[0])
+      var ip = ips.shift()
+      const nmap = spawn('nmap', ['-sS', '-A', '-O', '-R', '-Pn', '--top-ports', '1000', '-oX', xml, '-T', 'polite', ip])
+      inUse = ip
+      console.log('spawned nmap')
+      count++
+      nmap.setMaxListeners(20)
+      nmap.stderr.on('error', (err) => {
+        console.log(error)
+      })
+      nmap.on('close', (data) => {
+        count--
+      })
+    }
   }
-//},1000)
+}, 5000)
+
+
+/*setInterval(()=>{
+  var xmlFiles = fs.readdirSync(__dirname + '/xml/')
+  if(xmlFiles.length > 0){
+    for(var j = 0; j< xmlFiles.length; j++){
+      var unparsedXml = fs.readFileSync(__dirname + '/xml/' + xmlFiles[j])
+      processNmap(unparsedXml, (err, result)=>{
+        if(!err)
+        ws.send(JSON.stringify({type: 'scanResult', data: result}), (err)=>{
+          if(!err) fs.unlinkSync(__dirname + '/xml/' + xmlFiles[j])
+          else(console.log(err))
+        })
+      })
+    }
+  }
+},5000)*/
